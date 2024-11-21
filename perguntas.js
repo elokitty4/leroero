d4fce4b675b28178e3792a04d2b285bdfcaf2cf9
elo <00001126385554sp@al.educacao.sp.gov.br>
2024-11-21 08:51:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar 2024?',
+    'Oppenheimer'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual foi a melhor animação do Oscar 2024?',
+    'O Menino e a Garça'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual a melhor trilha sonora premiada no Oscar 2024?',
+    'Oppenheimer'
+)
+
+criaCartao(
+    'Cinema',
+    'Melhor ator e atriz premiado no Oscar 2024',
+    'O ator Cillian Murphy e a atriz Emma Stone'
+)
